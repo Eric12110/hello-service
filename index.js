7f58cbd6dac50a3c850d5411143e5693a1d866aa
Eric12110 <yes850520004@gmail.com>
2025-05-27 12:21:33 +0800

    init: Hello service CI/CD

---
+++ index.js
@@ -0,0 +1,5 @@
+﻿const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (_, res) => res.send("Hello from CI/CD!"));
+app.listen(port, () => console.log(`Server running on port ${port}`));
